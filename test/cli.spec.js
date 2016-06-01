e6aa01254f639444b760f66b4c2b142c97977987
fluidbyte <kent@fluidbyte.net>
2016-06-01 15:25:43 -0400

    Making test modification a bit more obvious

--- test/cli.spec.js
+++ test/cli.spec.js
@@ -5,9 +5,9 @@ const _ = require('lodash')
 describe('cli', () => {
   describe('args', () => {
     it('returns resources and config from arguments and default config', () => {
-      const argv = { _: [ 'foo', 'bar' ], t: 5000 }
+      const argv = { _: [ 'foo', 'bar' ], t: 1234 }
       const actual = cli.args(argv)
-      expect(actual).to.deep.equal({ resources: argv._, config: { timeout: 5000 } })
+      expect(actual).to.deep.equal({ resources: argv._, config: { timeout: 1234 } })
     })
   })
 })
