01f16ec5e552605258277ddff87725632426c250
fluidbyte <kent@fluidbyte.net>
2016-06-21 13:28:45 -0400

    Full coverage on core upyet lib

--- test/upyet.spec.js
+++ test/upyet.spec.js
@@ -47,4 +47,26 @@ describe('upyet', () => {
       return upyet.testResource('localhost:8989', defaultConfig).catch(() => true)
     })
   })
+  describe('run', () => {
+    let testService1
+    let testService2
+    after(() => {
+      testService1.close()
+      testService2.close()
+    })
+    it('throws an error if no resources are supplied', () => {
+      return upyet.run({ resources: [] }).catch(() => true)
+    })
+    it('runs check/s based on config object', () => {
+      testService1 = service(8989, 200)
+      testService2 = service(8990, 100)
+      return upyet.run({
+        resources: [ 'localhost:8989', 'localhost: 8990' ],
+        config: {
+          timeout: 100,
+          retries: 5
+        }
+      })
+    })
+  })
 })
